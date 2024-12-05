80c376deeb5781f2fa84415e77f0caf9800bc203
rrahmatnn <rizkyrahmat198@gmail.com>
2024-12-06 01:46:00 +0700

    fix(topup-list.tsx) : some text)

--- src/constan/topup-list/index.ts
+++ src/constan/topup-list/index.ts
@@ -1,26 +1,26 @@
 export const TopUpList = [
     {
-        tag : 'Rp. 10.0000',
+        tag : 'Rp. 10.000',
         nominal : 10000
     },
     {
-        tag : 'Rp. 20.0000',
+        tag : 'Rp. 20.000',
         nominal : 20000
     },
     {
-        tag : 'Rp. 50.0000',
+        tag : 'Rp. 50.000',
         nominal : 50000
     },
     {
-        tag : 'Rp. 100.0000',
+        tag : 'Rp. 100.000',
         nominal : 100000
     },
     {
-        tag : 'Rp. 250.0000',
+        tag : 'Rp. 250.000',
         nominal : 250000
     },
     {
-        tag : 'Rp. 500.0000',
+        tag : 'Rp. 500.000',
         nominal : 500000
     }
 ]
